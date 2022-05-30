ef8c2b4114530698213e144e72d3b9f2a8e418f5
highdanny11 <highdanny11@gmail.com>
2022-05-30 21:14:26 +0800

    上板

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,60 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    {{ xxx }}
   </div>
   <router-view />
 </template>
 
+<script>
+import { ref } from 'vue'
+// import mqtt from '@/worker/mqtt.js'
+const Stomp = require('stompjs')
+const SockJS = require('sockjs-client')
+
+export default {
+  setup() {
+    const url = 'http://101.32.168.101:8801/market/market-ws'
+    // let client = mqtt.connect(
+    //   'ws://101.32.168.101:8801',
+    //   {
+    //     incomingStore: 10000,
+    //     outgoingStore: 10000,
+    //     authenticationData: 'arraybuffer',
+    //   },
+    // )
+    // client.on('connect', function () {
+    //   console.log('Connected')
+    //   client.subscribe('/topic/market/thumb', function (err) {
+    //     if (!err) {
+    //       client.publish('test', 'Hello mqtt')
+    //     }
+    //   })
+    // })
+    // setTimeout(() => {
+    //   client.end()
+    // }, 5000)
+    const socket = new SockJS(url)
+    console.log(socket)
+    console.log(Stomp.over(socket))
+    const stompClient = Stomp.over(socket)
+    stompClient.connect({}, function (frame) {
+      console.log(frame)
+      const a = stompClient.subscribe('/topic/market/thumb', function (msg) {
+        console.log(msg)
+      })
+      setTimeout(() => {
+        a.unsubscribe()
+      }, 5000)
+    })
+    const xxx = ref('123')
+
+    return {
+      xxx,
+    }
+  },
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
